1339580e47fbc5707cfd5c252152a1a4e991cb25
aishathabdulla <60133917+aishathabdulla@users.noreply.github.com>
2021-03-17 16:30:08 +0000

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 class Navbar extends Component {
     render() {
       return (
-                <nav id= "navbar"class="navbar navbar-expand-xl navbar-light bg-light">
+                <nav id= "navbar"class="navbar navbar-expand-lg navbar-light bg-light">
                     <a class="navbar-brand" href="home.html"><strong><h2>FDM</h2></strong></a>
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
