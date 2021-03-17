88be0cd92391ac094600f257ad938527253aec30
aishathabdulla <60133917+aishathabdulla@users.noreply.github.com>
2021-03-17 17:44:03 +0000

    Add files via upload

---
+++ calendar.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import {render} from 'react-dom';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+
+const ReactCalendar = () => {
+    const [date, setDate] = useState(new Date());
+    const onChange = () => {
+        setDate(date);
+    }
+            return (
+                <div>
+                    <Calendar onChange={onChange} value={date}/>
+                </div>
+        )
+    }
+render(<ReactCalendar/>, document.querySelector("#root"));
