6d70019f7c0e6e05e3550a8920214002dbd66761
dharabh <80160351+dharabh@users.noreply.github.com>
2021-03-25 15:07:17 +0000

    Added tabs

--- src/components/directmessage.js
+++ src/components/directmessage.js
@@ -1,25 +1,50 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment} from 'react';
+import { render } from "react-dom";
 import MessageList from './messagelist';
 import SendMessage from './sendmessage';
 //https://www.freecodecamp.org/news/how-to-build-a-react-js-chat-app-in-10-minutes-c9233794642b/
+//https://stackoverflow.com/questions/54991227/how-to-create-vertical-tab-using-reactjs-and-css
 
 class DirectMessage extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
+      currentTab: -1,
       messages: [
-        {senderId: "bobross", text: 'hello'},
-        {senderId: "janedoe", text: 'hiya'}
+        {id:"1", senderId: "bobross", text: 'hello'},
+        {id:"2", senderId: "janedoe", text: 'hiya'},
+        {id:"3", senderId: "johnsmith", text: 'hey'}
       ]
-    }
+    };
+    this.handleClick = this.handleClick.bind(this);
   }
 
+  handleClick(currentTab) {
+    this.setState({ currentTab });
+  }
+
+//<MessageList messages={this.state.messages}/>
+//<SendMessage/>
   render() {
     return (
       <div className="dm">
         <h3>Direct Messages</h3>
-        <MessageList messages={this.state.messages}/>
-        <SendMessage/>
+        <div className="tab">
+          {this.state.messages.map((button, i) => (
+            <button key={button.senderId} className="tablinks" onClick={() => this.handleClick(i)}>{button.senderId}</button>
+            )
+          )
+          }
+        </div>
+        <div className="tabcontent">
+          {this.state.currentTab !== -1 &&
+            <React.Fragment>
+              <h5>{this.state.messages[this.state.currentTab].senderId}</h5>
+              <p>{this.state.messages[this.state.currentTab].text}</p>
+              <SendMessage/>
+            </React.Fragment>
+          }
+        </div>
       </div>
     );
   }
